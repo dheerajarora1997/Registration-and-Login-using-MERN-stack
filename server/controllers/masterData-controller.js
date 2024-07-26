66c42390768325699ada9ec8a5d269d70e21275f
dheeraj-arora_COMVIVA <dheeraj.arora1997@yho.com>
2024-07-26 18:05:43 +0530

    master data all added

--- server/controllers/masterData-controller.js
+++ server/controllers/masterData-controller.js
@@ -16,24 +16,18 @@ const getAllMasterData = async (req, res) => {
 };
 
 const getMasterDataByKey = async (req, res) => {
+  console.log(req.query, "req.query", req.params);
   const { key } = req.params;
+  const { parentCode } = req.query;
   try {
-    const allMasterData = await MasterData.find({ key: key });
-    res.status(200).send(allMasterData);
-  } catch (error) {
-    const err = {
-      status: 400,
-      message: error,
-      extraMessage: "Backend issue found!",
-    };
-    next(err);
-  }
-};
-
-const getMasterDataByParent = async (req, res) => {
-  const { value } = req.query;
-  try {
-    const allMasterData = await MasterData.find({ parentCode: value });
+    const query = {};
+    if (key) {
+      query.key = key;
+    }
+    if (parentCode) {
+      query.parentCode = parentCode;
+    }
+    const allMasterData = await MasterData.find(query);
     res.status(200).send(allMasterData);
   } catch (error) {
     const err = {
@@ -48,5 +42,4 @@ const getMasterDataByParent = async (req, res) => {
 module.exports = {
   getAllMasterData,
   getMasterDataByKey,
-  getMasterDataByParent,
 };
--- server/router/masterData-router.js
+++ server/router/masterData-router.js
@@ -4,9 +4,5 @@ const MasterDataControllers = require("../controllers/masterData-controller");
 
 router.route("/").get(MasterDataControllers.getAllMasterData);
 router.route("/:key").get(MasterDataControllers.getMasterDataByKey);
-router
-  .route("/:key?value=:value")
-  .get(MasterDataControllers.getMasterDataByParent);
-// router.route("/").post(MasterDataControllers.postAllMasterData);
 
 module.exports = router;
